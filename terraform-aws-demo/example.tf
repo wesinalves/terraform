012dd187199e2c8dc466b21d4be5d9cfbf376021
wesinalves <wesinec@gmail.com>
2021-02-14 15:56:19 -0300

    update aws example

---
+++ terraform-aws-demo/example.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-03d315ad33b9d49c4"
+  instance_type = "t2.micro"
+}
