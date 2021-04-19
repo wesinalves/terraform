7350f638ffbc0e0cc5fae1eb5cbb834448646c3b
wesinalves <wesinec@gmail.com>
2021-04-19 15:04:41 -0300

    test basis compartment

--- terraform-oracle/tf-compartment/compartment.tf
+++ terraform-oracle/tf-compartment/compartment.tf
@@ -4,5 +4,5 @@ resource "oci_identity_compartment" "tf-compartment" {
     # Required
     compartment_id = var.tenancy_ocid
     description = "Compartment for Terraform resources."
-    name = "mcom"
+    name = "basis"
 }
